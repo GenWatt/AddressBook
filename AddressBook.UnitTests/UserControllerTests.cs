8a01a85ecab355b5bccbf749eb3aaaf405a10e6e
GenWatt <adrian.kosoglos@gmail.com>
2024-05-24 18:57:48 +0200

    Change project structure, add unit test

---
+++ AddressBook.UnitTests/UserControllerTests.cs
@@ -0,0 +1,105 @@
+using System.Security.Claims;
+using AddressBook.Controllers;
+using AddressBook.Models;
+using AddressBook.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace AddressBook.Tests.Controllers;
+
+public class UserControllerTests
+{
+    [Fact]
+    public async Task AddAddress_WhenUserIsAuthenticated_ShouldCallAddAddressToUserAndRedirectToIndex()
+    {
+        // Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.AddAddressToUser(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
+
+        var controller = new UserController(userServiceMock.Object);
+        var userClaims = new ClaimsPrincipal(new ClaimsIdentity(new[]
+        {
+              new Claim(ClaimTypes.NameIdentifier, "currentUserId")
+        }, "mock"));
+
+        controller.ControllerContext = new ControllerContext()
+        {
+            HttpContext = new DefaultHttpContext() { User = userClaims }
+        };
+
+        // Act
+        var result = await controller.AddAddress("userToAddId") as RedirectToActionResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Index", result.ActionName);
+        Assert.Equal("Address", result.ControllerName);
+        userServiceMock.Verify(x => x.AddAddressToUser("currentUserId", "userToAddId"), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteAddress_WhenUserIsAuthenticated_ShouldCallDeleteAddressFromUserAndRedirectToIndex()
+    {
+        // Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.DeleteAddressFormUser(It.IsAny<string>(), It.IsAny<string>())).Returns(Task.CompletedTask);
+
+        var controller = new UserController(userServiceMock.Object);
+        var userClaims = new ClaimsPrincipal(new ClaimsIdentity(new[]
+        {
+                new Claim(ClaimTypes.NameIdentifier, "currentUserId")
+            }, "mock"));
+
+        controller.ControllerContext = new ControllerContext()
+        {
+            HttpContext = new DefaultHttpContext() { User = userClaims }
+        };
+
+        // Act
+        var result = await controller.DeleteAddress("userToDeleteId") as RedirectToActionResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal("Index", result.ActionName);
+        Assert.Equal("Address", result.ControllerName);
+        userServiceMock.Verify(x => x.DeleteAddressFormUser("currentUserId", "userToDeleteId"), Times.Once);
+    }
+
+    [Fact]
+    public async Task Details_WhenUserExists_ShouldReturnViewWithUser()
+    {
+        // Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(new UserModel { Id = "userId", FirstName = "John" });
+
+        var controller = new UserController(userServiceMock.Object);
+
+        // Act
+        var result = await controller.Details("userId") as ViewResult;
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.IsType<UserModel>(result.Model);
+        var model = result.Model as UserModel;
+        Assert.Equal("userId", model.Id);
+        Assert.Equal("John", model.FirstName);
+    }
+
+    [Fact]
+    public async Task Details_WhenUserDoesNotExist_ShouldReturnNotFound()
+    {
+        // Arrange
+        var userServiceMock = new Mock<IUserService>();
+        userServiceMock.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync((UserModel)null);
+
+        var controller = new UserController(userServiceMock.Object);
+
+        // Act
+        var result = await controller.Details("nonExistingUserId") as NotFoundResult;
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
+
